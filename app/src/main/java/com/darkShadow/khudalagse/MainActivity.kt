1bd9583c4f5713b13a802639d95a01732303214b
jahidRatul <jahidratul@gmail.com>
2023-02-23 17:59:45 +0600

    button edit text id toast

--- app/src/main/java/com/darkShadow/khudalagse/MainActivity.kt
+++ app/src/main/java/com/darkShadow/khudalagse/MainActivity.kt
@@ -2,10 +2,26 @@ package com.darkShadow.khudalagse
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val loginButton =findViewById<Button>(R.id.login_button)
+        val userPhone = findViewById<EditText>(R.id.user_phone)
+
+        loginButton.setOnClickListener{
+                val typedText= userPhone.text
+
+            Toast.makeText(this,typedText,Toast.LENGTH_SHORT).show()
+        }
+
     }
+
+
+
 }
